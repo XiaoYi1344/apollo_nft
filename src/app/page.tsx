4dbc3231cff5cf37a2655c3614c6efed4bba98e0
XiaoYi1344 <tieutyty1404@gmail.com>
2025-11-07 16:32:38 +0700

    Change link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 // import Page from '@/components/creator/page';
 import HomePage from '@/components/home/homePage';
-import { Upload } from '@/components/uploads/Upload';
+// import { Upload } from '@/components/uploads/Upload';
 // import BlogList from '@/components/test';
 // import TermsSlidingPanel from '@/components/term/term';
 
@@ -9,13 +9,13 @@ import { Upload } from '@/components/uploads/Upload';
 export default function Home() {
   return (
     <>
-      {/* <HomePage/> */}
+      <HomePage/>
       {/* <DropPage /> */}
       {/* <Marketplace /> */}
       {/* <TermsSlidingPanel /> */}
       {/* <Page /> */}
       {/* <BlogList /> */}
-      <Upload />
+      {/* <Upload /> */}
     </>
   );
 }
